e7968a7316be8ae489ac3db769139e9a88199493
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-07-01 14:19:36 +0200

    feat: update get-profile route

--- backend/src/routes/profile.rs
+++ backend/src/routes/profile.rs
@@ -1,12 +1,12 @@
 use rocket::serde::json::Json;
 use serde::{Serialize, Deserialize};
 use rocket::State;
+use sqlx::prelude::FromRow;
 use sqlx::PgPool;
 
 use crate::claims::Claims;
-use crate::models::user::User;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, FromRow)]
 pub struct UserProfile {
     pub username: String,
     pub keplr_address: Option<String>,
@@ -14,7 +14,7 @@ pub struct UserProfile {
 
 #[get("/profile")]
 pub async fn get_profile(user: Claims, pool: &State<PgPool>) -> Option<Json<UserProfile>> {
-    let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE username = $1")
+    let user: UserProfile = sqlx::query_as("SELECT * FROM users WHERE username = $1")
         .bind(&user.name)
         .fetch_one(pool.inner())
         .await
