52443ca4165f2ab35b42d69d952ca7f71f48160d
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-07-01 14:18:14 +0200

    feat: update user model to iterative version

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -1,12 +1,24 @@
 use serde::{Deserialize, Serialize};
-use sqlx::FromRow;
+use sqlx::{FromRow, Row};
+use chrono::NaiveDateTime;
 
-#[derive(Serialize, Deserialize, FromRow)]
+#[derive(Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
     pub username: String,
     pub password: Option<String>,
     pub keplr_address: Option<String>,
-    pub pfp_url: Option<String>,
-    pub created_at: chrono::NaiveDateTime,
+    pub created_at: Option<NaiveDateTime>,
+}
+
+impl<'r> FromRow<'r, sqlx::postgres::PgRow> for User {
+    fn from_row(row: &'r sqlx::postgres::PgRow) -> Result<Self, sqlx::Error> {
+        Ok(User {
+            id: row.try_get("id")?,
+            username: row.try_get("username")?,
+            password: row.try_get("password")?,
+            keplr_address: row.try_get("keplr_address")?,
+            created_at: row.try_get("created_at")?,
+        })
+    }
 }
