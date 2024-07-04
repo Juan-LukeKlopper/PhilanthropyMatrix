f6976ed60c0f051b8085f1c059b9db84fcc994fe
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-07-04 10:59:24 +0200

    feat: add minidenticon component

---
+++ frontend/src/lib/components/Minidenticons.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { minidenticon } from 'minidenticons';
+  
+    export let username = '';
+  
+    let svgHtml = '';
+  
+    $: svgHtml = minidenticon(username);
+  </script>
+  
+  <div class="minidenticon-container">
+    {@html svgHtml}
+  </div>
+  
+  <style>
+    .minidenticon-container {
+      border-radius: 50%;
+      background-color: grey;
+      height: 60px;
+      width: 60px;
+    }
+  </style>
+  
